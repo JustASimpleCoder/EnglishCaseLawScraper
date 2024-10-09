c26b6b5f69b5b57ef1231a33309d382dd5e50814
Jacob Cohen <thejacobcohen@gmail.com>
2024-10-09 11:17:15 -0400

    mad massive cchanges to CMake configuration

--- main.cpp
+++ main.cpp
@@ -7,8 +7,15 @@
 
 int main(){
 
-    
-    
+
+    // define the user agent for the GET request
+    cpr::Header headers = {{"User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}};
+    // make the HTTP request to retrieve the target page
+    cpr::Response response = cpr::Get(cpr::Url{"https://www.bailii.org/ew/cases/EWHC/Costs/2024/"}, headers);
+
+    std::cout << response.text.c_str() << std::endl;
+
+    // scraping logic...
 
 
 
